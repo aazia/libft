948dd4ee7ec75984e3f00fea8c5b6626efc0fb92
Azia Keever <azkeever@e1z2r7p23.42.us.org>
2018-08-27 00:15:00 -0700

    strchr

--- libft/libft.h
+++ libft/libft.h
@@ -3,12 +3,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-void      ft_putchar(char c);
-void      ft_putstr(char const *s);
-int       ft_tolower(int c);
-int       ft_toupper(int c);
-int       ft_strcmp(const char *s1, const char *s2);
-int       ft_strlen(const char *s);
-int       ft_isprint(int c);
-
+void		ft_putchar(char c);
+void		ft_putstr(char const *s);
+int			ft_tolower(int c);
+int			ft_toupper(int c);
+int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strlen(const char *s);
+int			ft_isprint(int c);
+char		*ft_strchr(const char *s, int c);
 #endif
--- libft/main.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-#include <stdio.h>
-int   main(int argc, char **argv)
-{
-  argc = 1;
-  printf("%d", ft_isprint(argv[1][0]));
-  return (0);
-}
