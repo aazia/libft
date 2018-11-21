4ec105808632dc5a8371170cb3997b1651e3dec2
aazia <azkeever@gmail.comn>
2018-11-21 19:46:26 +0100

    h

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,46 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: azkeever <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/20 14:52:05 by azkeever          #+#    #+#             */
-/*   Updated: 2018/11/20 22:14:38 by azkeever         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-char		*ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-	char	buff[22];
-	char	*str;
-	int		i;
-	int		ntmp;
+	int		len;
+	int		sign;
+	int		tmp;
+	char	*a;
 
-	i = 0;
-	ntmp = n;
+	sign = 0;
 	if (n < 0)
-		ntmp = -n;
-	while (ntmp > 0)
-	{
-		buff[i++] = (ntmp % 10) + 48;
-		ntmp = ntmp / 10;
-	}
-	if (n < 0)
-	{
-		buff[i++] = '-';
-		if (!(str = ft_memalloc((size_t)i + 2)))
-			return (NULL);
-	}
-	else if (!(str = ft_memalloc((size_t)i + 1)))
+		sign = 1;
+	tmp = n;
+	len = 1;
+	while ((tmp /= 10))
+		len++;
+	a = ft_strnew(len + sign);
+	if (a == NULL)
 		return (NULL);
-	if (n == -2147483648)
-		return ("-2147483648");
-	else if (n == 0)
-		str[0] = '0';
-	while (--i >= 0)
-		str[ntmp++] = buff[i];
-	return (str);
+	if (sign)
+		a[0] = '-';
+	while (len--)
+	{
+		a[len + sign] = (sign ? -(n % 10) : (n % 10)) + '0';
+		n /= 10;
+	}
+	return (a);
 }
